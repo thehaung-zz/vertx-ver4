5bddb37b4be9a3ef94ff0aae1a3f28343b64d3ae
Hau Nguyen <thehaung217@gmail.com>
2022-06-26 08:34:37 +0700

    build: add main verticle for builder

--- src/main/java/vn/thehaung/Main.java
+++ src/main/java/vn/thehaung/Main.java
@@ -1,9 +1,15 @@
 package vn.thehaung;
 
+import io.vertx.core.Vertx;
+import vn.thehaung.verticle.MainVerticle;
+
 /**
  * @author : Hau Nguyen
  * @created : 6/26/22
  **/
 
 public class Main {
+  public static void main(String[] args) {
+    Vertx.vertx().deployVerticle(new MainVerticle());
+  }
 }
